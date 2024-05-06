cb48a9bf883952c9877f0725be9841c5086e1af7
saravanasandy <msaravanakumar369@gmail.com>
2024-05-06 19:11:37 +0530

    new change

--- src/Components/FlightItem.js
+++ src/Components/FlightItem.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { deleteFlight } from '../features/flights/flightSlice'
+<<<<<<< HEAD
 import {FaRegCalendarMinus} from 'react-icons/fa'
+=======
+>>>>>>> 22a387a62c39bbe03a2ff44795b293466bd454f1
 
 const FlightItem = ({flight}) => {
     const dispatch = useDispatch()
@@ -12,6 +15,7 @@ const FlightItem = ({flight}) => {
 
   return (
     <>
+<<<<<<< HEAD
     <div className='flight-details-list'>
         <div>
           <h4>Booking Time</h4>
@@ -48,6 +52,20 @@ const FlightItem = ({flight}) => {
         
        
         <button className='close' onClick={()=>dispatch(deleteFlight(flight._id))}> X</button>
+=======
+    <div className='goal'>
+        <div>
+            {new Date(flight.createdAt).toLocaleString('en-US')}
+        </div>
+        
+        {/* <h3>{flight._id}</h3> */}
+        <h2>{flight.Destination_From}</h2>
+        <h2>{flight.Destination_To}</h2>
+        <h2>{flight.Journey_Date}</h2>
+        <h2>{flight.Guests}</h2>
+        <h2>{flight.ClassType}</h2>
+        <button className='close' onClick={()=>dispatch(deleteFlight(flight._id))}>X</button>
+>>>>>>> 22a387a62c39bbe03a2ff44795b293466bd454f1
     </div>
     </>
   )
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+<<<<<<< HEAD
+=======
+import goalReducer from '../features/goals/goalSlice';
+>>>>>>> 22a387a62c39bbe03a2ff44795b293466bd454f1
 import flightReducer from '../features/flights/flightSlice';
 
 
@@ -7,6 +11,10 @@ import flightReducer from '../features/flights/flightSlice';
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+<<<<<<< HEAD
+=======
+    goals: goalReducer,
+>>>>>>> 22a387a62c39bbe03a2ff44795b293466bd454f1
     flights: flightReducer
   },
 });
